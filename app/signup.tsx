b4373f2ee3f837c752448946e853048161263893
Suvdip12 <mahatosuvadip20@gmail.com>
2025-06-24 17:49:04 +0530

    add home screen

---
+++ app/signup.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { ImageBackground, Text, View } from 'react-native';
+import './global.css'; // Ensure you have global styles if needed
+
+const Signup = () => {
+  return (
+      <ImageBackground
+      source={require('../assets/images/Frame.webp')} // or { uri: 'https://...' }
+      className="flex-1 justify-center items-center"
+      resizeMode="cover"
+    >
+      <View className="bg-black/50 p-6 rounded-lg">
+        <Text className="text-white text-xl font-bold">Hello from Sign Up</Text>
+      </View>
+    </ImageBackground>
+  )
+}
+
+export default Signup;
